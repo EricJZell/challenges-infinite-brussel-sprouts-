a5b0c49d682693e3e4146f07e429bc0ca2b0466d
Eric Zell <EricJZell@gmail.com>
2015-08-20 13:50:39 -0400

    Added functionality so that new tweets load when the user has scrolled to the bottom of the page

--- public/js/sprouts.js
+++ public/js/sprouts.js
@@ -1,13 +1,16 @@
 // YOUR CODE GOES HERE
 var link = $('.more-sprouts');
 var ul = $('.tweets');
-var page = 1;
-link.click(function() {
-  event.preventDefault();
-  page ++;
-  $.get("/tweets.json?page=" + page, function(tweets) {
-    for (var i = 0; i < tweets.length; i++){
-      $('ul.tweets').append( "<li class='tweet'><div class='body'>" + tweets[i]["text"] + "</div><div class='user'>" + tweets[i]["username"] + "</div></li>" );
-    }
-  });
+var nextPage = 2;
+$(window).scroll(function() {
+   if($(window).scrollTop() + window.innerHeight == $(document).height()) {
+     $.get("/tweets.json?page=" + nextPage, function(tweets) {
+       if ( tweets.length > 0 ){
+         nextPage ++;
+       }
+       for (var i = 0; i < tweets.length; i++){
+         $('ul.tweets').append( "<li class='tweet'><div class='body'>" + tweets[i]["text"] + "</div><div class='user'>" + tweets[i]["username"] + "</div></li>" );
+       }
+     });
+   }
 });
